277f963e6205e050305317eb33903300f658dd59
Aravindan Balan <arbalan@paypal.com>
2016-07-16 20:41:36 -0700

    Review comment changes

--- Nanodegree-0/app/src/main/java/com/android/nanodegreeproject0/AppPortfolioActivity.java
+++ Nanodegree-0/app/src/main/java/com/android/nanodegreeproject0/AppPortfolioActivity.java
@@ -3,7 +3,6 @@ package com.android.nanodegreeproject0;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 public class AppPortfolioActivity extends AppCompatActivity {
@@ -12,68 +11,36 @@ public class AppPortfolioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_portfolio);
+    }
 
-        Button popular_movies_button = (Button) findViewById(R.id.popular_movies);
-        Button stock_hawk_button = (Button) findViewById(R.id.stock_hawk);
-        Button build_bigger_button = (Button) findViewById(R.id.build_bigger);
-        Button material_app_button = (Button) findViewById(R.id.material_app);
-        Button ubiquitous_app_button = (Button) findViewById(R.id.go_ubiquitous);
-        Button capstone_button = (Button) findViewById(R.id.capstone);
-
-        MyOnclickListener listener = new MyOnclickListener();
-        if (popular_movies_button != null) {
-            popular_movies_button.setOnClickListener(listener);
-        }
-        if (stock_hawk_button != null) {
-            stock_hawk_button.setOnClickListener(listener);
-        }
-        if (build_bigger_button != null) {
-            build_bigger_button.setOnClickListener(listener);
-        }
-        if (material_app_button != null) {
-            material_app_button.setOnClickListener(listener);
-        }
-        if (ubiquitous_app_button != null) {
-            ubiquitous_app_button.setOnClickListener(listener);
-        }
-        if (capstone_button != null) {
-            capstone_button.setOnClickListener(listener);
+    public void showToastMessage(View view) {
+        switch (view.getId()) {
+            case R.id.popular_movies:
+                renderToast(getString(R.string.button_popular_movies_text));
+                break;
+            case R.id.stock_hawk:
+                renderToast(getString(R.string.button_stock_hawk_text));
+                break;
+            case R.id.build_bigger:
+                renderToast(getString(R.string.button_bigger_text));
+                break;
+            case R.id.material_app:
+                renderToast(getString(R.string.button_material_app_text));
+                break;
+            case R.id.go_ubiquitous:
+                renderToast(getString(R.string.button_ubiquitous_app_text));
+                break;
+            case R.id.capstone:
+                renderToast(getString(R.string.button_capstone_app_text));
+                break;
         }
     }
 
-    private class MyOnclickListener implements View.OnClickListener {
+    private void renderToast(String message) {
         String toast_message = "";
         int duration = Toast.LENGTH_SHORT;
-
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.popular_movies:
-                    toast_message = getString(R.string.toast_message, getString(R.string.button_popular_movies_text));
-                    Toast.makeText(getApplicationContext(), toast_message, duration).show();
-                    break;
-                case R.id.stock_hawk:
-                    toast_message = getString(R.string.toast_message, getString(R.string.button_stock_hawk_text));
-                    Toast.makeText(getApplicationContext(), toast_message, duration).show();
-                    break;
-                case R.id.build_bigger:
-                    toast_message = getString(R.string.toast_message, getString(R.string.button_bigger_text));
-                    Toast.makeText(getApplicationContext(), toast_message, duration).show();
-                    break;
-                case R.id.material_app:
-                    toast_message = getString(R.string.toast_message, getString(R.string.button_material_app_text));
-                    Toast.makeText(getApplicationContext(), toast_message, duration).show();
-                    break;
-                case R.id.go_ubiquitous:
-                    toast_message = getString(R.string.toast_message, getString(R.string.button_ubiquitous_app_text));
-                    Toast.makeText(getApplicationContext(), toast_message, duration).show();
-                    break;
-                case R.id.capstone:
-                    toast_message = getString(R.string.toast_message, getString(R.string.button_capstone_app_text));
-                    Toast.makeText(getApplicationContext(), toast_message, duration).show();
-                    break;
-            }
-        }
+        toast_message = getString(R.string.toast_message, message);
+        Toast.makeText(getApplicationContext(), toast_message, duration).show();
     }
 }
 
